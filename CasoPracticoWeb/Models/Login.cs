1c55703d9098362ca0ee8d6bfe91045ae2379e3a
Jaible <105011906+Jaible@users.noreply.github.com>
2024-06-07 22:07:04 -0600

    Agregar archivos de proyecto.

---
+++ CasoPracticoWeb/Models/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace CasoPracticoWeb.Models
+{
+    public class Login
+    {
+        public required string Name { get; set; }
+        public required string Password { get; set; }
+    }
+}
