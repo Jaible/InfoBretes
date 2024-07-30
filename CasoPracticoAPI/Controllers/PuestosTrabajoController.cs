bb617dad159edaf8043ec153a8a305592f11e826
AGiart <117540143+AGiart@users.noreply.github.com>
2024-07-30 15:13:18 -0600

    administracion de ofertas y solicitudes

---
+++ CasoPracticoAPI/Controllers/PuestosTrabajoController.cs
@@ -0,0 +1,194 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using CasoPracticoAPI.Entities;
+using System.Data;
+using System.Data.SqlClient;
+using static CasoPracticoAPI.Entities.PuestosTrabajoEnt;
+
+namespace InfoBretesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PuestoTrabajoController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public PuestoTrabajoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [AllowAnonymous]
+        [Route("RegistrarPuestosTrabajo")]
+        [HttpPost]
+        public IActionResult RegistrarPuestosTrabajo(PuestosTrabajoEnt PuestosTrabajo)
+        {
+
+            PuestosTrabajoRespuesta PuestosTrabajoRespuesta = new PuestosTrabajoRespuesta();
+            try
+            {
+                using (var db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+                {
+                    var parametros = new
+                    {
+                        PuestosTrabajo.idEmpresa,
+                        PuestosTrabajo.titulo,
+                        PuestosTrabajo.descripcion,
+                        PuestosTrabajo.requisitos
+                    };
+
+                    var result = db.Execute("RegistrarOferta", parametros, commandType: CommandType.StoredProcedure);
+                    if (result > 0)
+                    {
+                        PuestosTrabajoRespuesta.Codigo = "1";
+                        PuestosTrabajoRespuesta.Mensaje = "PuestosTrabajo registrado con éxito.";
+                        return Ok(PuestosTrabajoRespuesta);
+                    }
+                    else
+                    {
+                        PuestosTrabajoRespuesta.Codigo = "-1";
+                        PuestosTrabajoRespuesta.Mensaje = "No se pudo registrar el PuestosTrabajo.";
+                        return BadRequest(PuestosTrabajoRespuesta);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Ocurrió un error al registrar el PuestosTrabajo.", error = ex.Message });
+            }
+        }
+        [AllowAnonymous]
+        [Route("ConsultarPuestosTrabajos")]
+        [HttpGet]
+        public IActionResult ConsultarPuestosTrabajo()
+        {
+            PuestosTrabajoRespuesta PuestosTrabajoRespuesta = new PuestosTrabajoRespuesta();
+            try
+            {
+                using (var db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+                {
+                    var resultadoBD = db.Query<PuestosTrabajoEnt>("ConsultarOferta", new { }, commandType: CommandType.StoredProcedure).ToList();
+
+                    if (resultadoBD == null || resultadoBD.Count == 0)
+                    {
+                        PuestosTrabajoRespuesta.Codigo = "-1";
+                        PuestosTrabajoRespuesta.Mensaje = "No hay Puestos de Trabajos registrados.";
+                    }
+                    else
+                    {
+                        PuestosTrabajoRespuesta.Datos = resultadoBD;
+                        PuestosTrabajoRespuesta.Codigo = "1";
+                        PuestosTrabajoRespuesta.Mensaje = "Puestos de Trabajos consultados con éxito.";
+                    }
+                    return Ok(PuestosTrabajoRespuesta);
+                }
+            }
+            catch (SqlException ex)
+            {
+
+                return StatusCode(500, new { message = "Error al consultar PuestosTrabajo en la base de datos.", error = ex.Message });
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, new { message = "Ocurrió un error inesperado al consultar PuestosTrabajo.", error = ex.Message });
+            }
+        }
+
+
+        [AllowAnonymous]
+        [Route("ConsultarUnPuestoTrabajo")]
+        [HttpGet]
+        public IActionResult ConsultarUnPuestoTrabajo(long idPuesto)
+        {
+            PuestosTrabajoRespuesta PuestosTrabajoRespuesta = new PuestosTrabajoRespuesta();
+            try
+            {
+                using (var db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+                {
+                    var result = db.Query<PuestosTrabajoEnt>("ObtenerOfertaPorId",
+                        new { idPuesto },
+                        commandType: CommandType.StoredProcedure).FirstOrDefault();
+
+                    if (result == null)
+                    {
+                        PuestosTrabajoRespuesta.Codigo = "-1";
+                        PuestosTrabajoRespuesta.Mensaje = "No hay PuestosTrabajo registrados.";
+                    }
+                    else
+                    {
+                        PuestosTrabajoRespuesta.Dato = result;
+                        PuestosTrabajoRespuesta.Codigo = "1";
+                        PuestosTrabajoRespuesta.Mensaje = "PuestosTrabajo consultado con éxito.";
+                    }
+
+                    return Ok(PuestosTrabajoRespuesta);
+                }
+            }
+            catch (SqlException ex)
+            {
+
+                return StatusCode(500, new { message = "Error al consultar el PuestosTrabajo en la base de datos.", error = ex.Message });
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, new { message = "Ocurrió un error inesperado al consultar el PuestosTrabajo.", error = ex.Message });
+            }
+        }
+
+
+
+
+        [AllowAnonymous]
+        [Route("EliminarPuestosTrabajo")]
+        [HttpDelete]
+        public IActionResult EliminarPuestosTrabajo(long idPuesto)
+        {
+            PuestosTrabajoRespuesta PuestosTrabajoRespuesta = new PuestosTrabajoRespuesta();
+            try
+            {
+                using (var db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+                {
+                    var result = db.Execute("EliminarOferta",
+                        new
+                        {
+                            idPuesto
+                        },
+                        commandType: CommandType.StoredProcedure);
+
+                    if (result <= 0)
+                    {
+                        PuestosTrabajoRespuesta.Codigo = "-1";
+                        PuestosTrabajoRespuesta.Mensaje = "No se ha podido eliminar el PuestosTrabajo en la base de datos, intenta de nuevo";
+                        return BadRequest(PuestosTrabajoRespuesta);
+                    }
+                    else
+                    {
+                        PuestosTrabajoRespuesta.Codigo = "1";
+                        PuestosTrabajoRespuesta.Mensaje = "PuestosTrabajo eliminado con éxito.";
+                        return Ok(PuestosTrabajoRespuesta);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+
+                return StatusCode(500, new { message = "Error al eliminar el PuestosTrabajo en la base de datos.", error = ex.Message });
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, new { message = "Ocurrió un error inesperado al eliminar el PuestosTrabajo.", error = ex.Message });
+            }
+        }
+
+
+
+
+    }
+}
+
+
---
+++ CasoPracticoAPI/Entities/PuestosTrabajoEnt.cs
@@ -0,0 +1,29 @@
+﻿namespace CasoPracticoAPI.Entities
+{
+    public class PuestosTrabajoEnt
+    {
+        public long idPuesto { get; }
+        public int idEmpresa { get; set; }
+        public string? titulo { get; set; }
+        public string? descripcion { get; set; }
+        public string? requisitos { get; set; }
+        public string? fechaPublicacion { get; }
+        public string? fechaCierre { get; }
+
+        public class PuestosTrabajoRespuesta
+        {
+            public PuestosTrabajoRespuesta()
+            {
+                Codigo = "1";
+                Mensaje = string.Empty;
+                Dato = null;
+                Datos = null;
+            }
+
+            public string Codigo { get; set; }
+            public string Mensaje { get; set; }
+            public PuestosTrabajoEnt? Dato { get; set; }
+            public List<PuestosTrabajoEnt>? Datos { get; set; }
+        }
+    }
+}
